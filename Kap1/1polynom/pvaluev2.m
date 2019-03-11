44a63bb0b5f48d3385cbb6fa35e30fac5e84dd39
surfacebook <jakobreichl@gmail.com>
2019-03-11 17:23:24 +0100

    added testfunctions for pvalue

--- Kap1/1polynom/pvaluev2.m
+++ Kap1/1polynom/pvaluev2.m
@@ -2,7 +2,7 @@ function y = pvaluev2( a, x )
    if ~ ( isempty(a) || isvector(a) )
       error("No valid polynom");
    end
-   y = zeros(size(a));
+   y = zeros(size(x));
    n = length(a);
    for k=1:n
       y = y + a(k)*x.^(n-k);
---
+++ Kap1/1polynom/pvaluev2Test.m
@@ -0,0 +1,15 @@
+function pvaluev2Test()
+  x=[2.3, 3.4];
+  checkPvalue([1, -4, 0, 7],x)
+  checkPvalue([],x)
+  checkPvalue([0],x)
+  checkPvalue([1, -4, 0, 7],[])
+  checkPvalue([],[])  
+end
+
+function checkPvalue(a,x)
+  disp('**************************')
+  disp(strcat('a=',mat2str(a),' x=',mat2str(x)))
+  pvaluev2Erg= pvaluev2(a,x)
+  polyvalErg = polyval(a,x)
+end
