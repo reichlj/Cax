57b7de395be28478bfa48067f1df02f513bdeb09
jakob <jakobreichl@gmail.com>
2019-04-02 18:40:34 +0200

    Beispiele Zeiger und Vektoren

---
+++ Kap4/2AufrufParameter/csum.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+double myAdd(double a, double b);
+
+int main(int argc, char *argv[])
+{   
+   double a,b,c;
+   if ( argc != 3) {
+     printf("Fehler\n");
+     printf("  Aufruf: csum a b");
+     return -1;
+   }
+   printf("%s\n", argv[0]);
+   a = strtod(argv[1],0);
+   b = strtod(argv[2],0);
+   c = myAdd(a,b);
+   printf("%.2f+%.2f=%.2f\n",a,b,c);
+   return 0;
+}
+
+double myAdd(double a, double b)
+{
+  double sum;
+  sum = a + b;
+  return sum;
+}
+
