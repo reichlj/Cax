ca32ac20d6e2e3111be4e7011ef777fe55bfbe15
jakob <jakobreichl@gmail.com>
2019-06-08 15:51:38 +0200

    Kap6 CallbackFunktionen

---
+++ Kap6/1CallBack/setParamKette3m.m
@@ -0,0 +1,9 @@
+clear all
+% Modellparameter
+m = 2.0;
+m1 = m; m2 = m; m3 = m;
+c = 1;
+c1 = c; c2 = c; c3 = c; c4 = c;
+% Anfangsbedingungen
+x10 = -1; x20 = 0; x30 = 1;
+v10 = 0;  v20 = 0; v30 = 0;
