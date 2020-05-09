32f93cdc2e18e93a415060f2683ed3844db11a31
reichlj <jakobreichl@gmail.com>
2020-05-09 15:42:41 +0200

    Bat-Datei von Qt 12.2 auf Qt 14.2 umgestellt

--- Kap2/anwendungen/plotsincos1.m
+++ Kap2/anwendungen/plotsincos1.m
@@ -4,3 +4,4 @@ y = sin(x);
 z = cos(x);
 plot(x,y, x,z, 'LineWidth',3)
 h = legend('Sinus', 'Cosinus')
+%h = legend({'Sinus', 'Cosinus'})
--- Kap4/1AcmdQt/Qtsetup32.bat
+++ Kap4/1AcmdQt/Qtsetup32.bat
@@ -1,3 +1,3 @@
 echo 32-Bit gcc einbinden
-set QT=C:\Qt\Qt5.12.2
+set QT=C:\Qt\Qt5.14.2
 set PATH=%QT%\Tools\mingw730_32\bin;%PATH%
