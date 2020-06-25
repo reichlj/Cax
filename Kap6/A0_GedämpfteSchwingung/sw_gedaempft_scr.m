ba5cbef6b3bfa274c13e0d7fc4b3c42c7a266a79
reichlj <jakobreichl@gmail.com>
2020-06-25 11:49:11 +0200

    Kap6 Aufgabe 0

---
+++ Kap6/A0_GedämpfteSchwingung/sw_gedaempft_scr.m
@@ -0,0 +1,17 @@
+clear all
+simOut = sim('swGedaempft',...
+             'SaveTime','on', ...
+             'SaveOutput','on', ...
+             'SaveFormat','Array');
+t = simOut.get('tout');         
+y = simOut.get('yout');  
+vsum = 0.0;
+index = 1;
+while t(index) < 5.0
+    vsum = vsum + y(index,2);
+    index = index + 1;
+end
+vd= vsum/index
+fprintf('Durchschnittsgeschwindigkeit vd=%8.4f\n',vd);
+
+         
