4cb74a7860c81edba9b981ae6a15a8d135b8f6f9
reichlj <jakobreichl@gmail.com>
2021-05-12 21:34:23 +0200

    Delete Kap3Try und Kap3Map

--- Kap3Map/durchschnitt2.m
+++
@@ -1,8 +0,0 @@
-function [avg] = durchschnitt2(rainmap)
-  myvalues = rainmap.values();
-  val = 0.0;
-  for k=1:rainmap.Count
-      val = val + myvalues{k};
-  end
-  avg = val/rainmap.length();
-end
